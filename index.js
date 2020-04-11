abb783f489afbb364f7c27c68d861323cab2ca09
aman-atg <amanansari.a2@gmail.com>
2020-04-11 15:24:24 +0530

    explode time

--- index.js
+++ index.js
@@ -28,26 +28,17 @@ function draw() {
     ship.thrust.x -= (FRICTION * ship.thrust.x) / FPS;
     ship.thrust.y -= (FRICTION * ship.thrust.y) / FPS;
   }
-  if (!exploding) drawAirship();
-  else {
+  if (!exploding) {
+    drawAirship();
+    moveShip();
+  } else {
     drawExplosion();
   }
+  ship.explodeTime--;
+  if(ship.explodeTime==0)
+    ship = newAirship();
 
-  // handle edges of the screen
-  if (ship.x < 0 - ship.r) ship.x = width + ship.r;
-  else if (ship.x > width + ship.r) ship.x = 0 - ship.r;
-  if (ship.y < 0 - ship.r) ship.y = height + ship.r;
-  else if (ship.y > height + ship.r) ship.y = 0 - ship.r;
-
-  // rotate the ship
-  ship.a += ship.rot;
-
-  // move the ship
-  const { x, y } = ship.thrust;
-  ship.x += x;
-  ship.y += y;
-
-  handleAsteroids();
+  handleAsteroids(exploding);
 
   if (keyIsPressed) checkKeys();
 }
