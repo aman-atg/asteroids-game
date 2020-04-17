6f5ddd7ee9f8ce5d375a514abb8440ce345589b0
Aman Ansari <amanansari.a2@gmail.com>
2020-04-17 18:19:53 +0530

    Update Index.js
    remove bugs and implement p5.js loadSound func

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,23 +8,27 @@ var Text;
 var score = 0;
 var highScore;
 // var pause = false;
-var noSound = true;
+var noSound = false;
 
-let explode_S, hit_S, laser_S, music_high_S, music_low_S, thrust_S;
+let explode_S, laser_S, music_high_S, music_low_S, thrust_S;
 
 function preload() {
-    explode_S = createAudio("./../../public/sounds/explode.m4a");
-    hit_S = createAudio("./../../public/sounds/hit.m4a");
-    laser_S = createAudio("./../../public/sounds/laser.m4a");
-    music_high_S = createAudio("./../../public/sounds/music-high.m4a");
-    music_low_S = createAudio("./../../public/sounds/music-low.m4a");
-    thrust_S = createAudio("./../../public/sounds/thrust.m4a");
+  explode_S = loadSound("./../../public/sounds/explode.m4a");
+  hit_S = loadSound("./../../public/sounds/hit.m4a");
+  laser_S = loadSound("./../../public/sounds/laser.m4a");
+    music_high_S = loadSound("./../../public/sounds/music-high.m4a");
+    music_low_S = loadSound("./../../public/sounds/music-low.m4a");
+    thrust_S = loadSound("./../../public/sounds/thrust.m4a");
+
+    laser_S.playMode('sustain')
+    laser_S.setVolume(1);
+
  
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight - 25);
-
+// music_low_S.loop(); //wroking fine
   H_width = width / 2;
   H_height = height / 2;
   newGame();
@@ -39,11 +43,19 @@ function draw() {
   stroke(255);
 
   if (ship.thrusting && !ship.dead) {
-    if (blinking) drawThruster();
-    thrust_S.play();
+    if (blinking) 
+    {
+    drawThruster();
+    if(!thrust_S.isPlaying())
+    thrust_S.loop();    
+    }
+
+  
     ship.thrust.x += (SHIP_THRUST * cos(ship.a)) / FPS;
     ship.thrust.y -= (SHIP_THRUST * sin(ship.a)) / FPS;
   } else {
+    thrust_S.stop();
+
     ship.thrust.x -= (FRICTION * ship.thrust.x) / FPS;
     ship.thrust.y -= (FRICTION * ship.thrust.y) / FPS;
   }
@@ -77,4 +89,9 @@ function draw() {
   if (keyIsPressed && !exploding && !ship.dead) checkKeys();
 
   drawText(exploding);
+
+  // if(ship.thrusting)    thrust_S.play();
+  // else thrust_S.stop();
+
+
 }
